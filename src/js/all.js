819c416a93b716f0c2abd4cf9e18246152caee28
Drew Sarette <103331340+drew-sarette@users.noreply.github.com>
2023-04-04 17:29:02 -0400

    Use JS rather than checkbox for menu; Add menu animations

--- src/js/all.js
+++ src/js/all.js
@@ -2,3 +2,10 @@ const currentPageLink = document.querySelector(`a[href*="${location.pathname}"]`
 currentPageLink.classList.add("current-page-a");
 currentPageLink.parentElement.classList.add("current-page-li");
 
+
+const menuViolinBow = document.querySelectorAll(".main-menu-label svg");
+const nav = document.getElementById("main-nav")
+document.querySelector(".main-menu-label").addEventListener("click", () => {
+    menuViolinBow.forEach(svg => svg.classList.toggle("open"));
+    nav.classList.toggle("open");
+})
