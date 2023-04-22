1c6122125c310d0105010742529177170bce7225
Drew Sarette <103331340+drew-sarette@users.noreply.github.com>
2023-04-22 16:28:11 -0400

    Remove unnecessary passthroughs in config

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,25 @@
 const { DateTime } = require("luxon");
+const CleanCSS = require("clean-css");
+
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
 
   eleventyConfig.addShortcode('currentYear', () => new Date().getFullYear());
  
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
 
-  //Add filter for luxon 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
-  // Define the "posts" collection
   eleventyConfig.addCollection("posts", function(collection) {
     return collection.getFilteredByGlob("./blog/*.md");
   });
 
-  // Define the "tutorials" collection
   eleventyConfig.addCollection("tutorials", function(collection) {
     return collection.getFilteredByGlob(".tutorials/*.md");
   });
