e9762434a48d676bb9fe20acc0c930de15c62304
Drew Sarette <103331340+drew-sarette@users.noreply.github.com>
2023-05-06 12:00:42 -0400

    Fix vimeo filter, in sync with origin main

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,14 @@
 const { DateTime } = require("luxon");
 const CleanCSS = require("clean-css");
 
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
-  eleventyConfig.addPassthroughCopy({ "./src/favicon" : "/" });
+  eleventyConfig.addPassthroughCopy({ "./src/favicon": "/" });
 
-  eleventyConfig.addShortcode('currentYear', () => new Date().getFullYear());
- 
-  eleventyConfig.addFilter("cssmin", function(code) {
+  eleventyConfig.addShortcode("currentYear", () => new Date().getFullYear());
+
+  eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
 
@@ -17,14 +16,13 @@ module.exports = function (eleventyConfig) {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
-  // eleventyConfig.addCollection("posts", function(collection) {
-  //   return collection.getFilteredByGlob("./blog/*.md");
-  // });
+  eleventyConfig.addFilter("vimeoEmbed", (vimeoId, title) => {
+    return `<figure style="padding:56.25% 0 0 0;position:relative;" class="vimeo-embed">
+    <iframe src="https://player.vimeo.com/video/${vimeoId}?h=16f6a7ef75&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen="allowfullscreen" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="${title}"></iframe>
+</figure>
+<script src="https://player.vimeo.com/api/player.js"></script>`;
+  });
 
-  // eleventyConfig.addCollection("tutorials", function(collection) {
-  //   return collection.getFilteredByGlob(".tutorials/*.md");
-  // });
-    
   return {
     dir: {
       input: "src",
