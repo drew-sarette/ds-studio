d7afbdefac8994ef2c33042bae045d376a2891ed
Drew Sarette <103331340+drew-sarette@users.noreply.github.com>
2023-05-19 12:44:56 -0400

    Add config file

--- .eleventy.js
+++ .eleventy.js
@@ -13,7 +13,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter("vimeoEmbed", (vimeoId, title) => {
     return `<figure style="padding:56.25% 0 0 0;position:relative;" class="vimeo-embed">
-    <iframe src="https://player.vimeo.com/video/${vimeoId}?h=16f6a7ef75&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen="allowfullscreen" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="${title}"></iframe>
+    <iframe loading="lazy" src="https://player.vimeo.com/video/${vimeoId}?h=16f6a7ef75&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen="allowfullscreen" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="${title}"></iframe>
 </figure>
 <script src="https://player.vimeo.com/api/player.js"></script>`;
   });
